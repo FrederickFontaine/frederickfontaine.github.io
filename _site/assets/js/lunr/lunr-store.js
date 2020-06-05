1c59be4ea66db883ab29fbb803a1a64d7b080bc7
Frederick Fontaine <frederick.fontaine@univ-lorraine.fr>
2020-06-05 15:28:09 +0200

    Test de MAJ

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Anaëlle Hily",
-        "excerpt":"Doctorante CIFRE avec TEA - rattache au projet N-Hum-Inno. Ses travaux portent sur …   ","categories": [],
-        "tags": ["Acceptability assessment","User eXperience","Living Lab"],
+        "excerpt":"Anaëlle is an industrial Ph.D. student at the University of Lorraine (ERPI) in collaboration with the company TEA. Her work intends to provide protocols and tools to study the interactions (behaviors, emotions) between participants facing immersive reality experiences. The developed tools should provide indicators for the analysis of these experiences...","categories": [],
+        "tags": ["VR","spatial cognition","Living Lab"],
         "url": "http://localhost:4000/people/anaelle-hily/",
         "teaser":"http://localhost:4000/assets/images/people/AH.png"},{
         "title": "Benjamin Enneser-Serville",
@@ -15,8 +15,8 @@ var store = [{
         "url": "http://localhost:4000/people/Giovanny-Arbelaez/",
         "teaser":"http://localhost:4000/assets/images/people/GA.jpg"},{
         "title": "Jean-Marie Diroux",
-        "excerpt":" ","categories": [],
-        "tags": ["Acceptability assessment","User eXperience","Living Lab"],
+        "excerpt":"JMD is the product director of TEA.  ","categories": [],
+        "tags": ["Direction"],
         "url": "http://localhost:4000/people/JM-Diroux/",
         "teaser":"http://localhost:4000/assets/images/people/JMD.png"},{
         "title": "Jonay Grandmontagne",
@@ -25,8 +25,8 @@ var store = [{
         "url": "http://localhost:4000/people/jonay-grandmontagne/",
         "teaser":"http://localhost:4000/assets/images/people/JG.png"},{
         "title": "Julien Michaux",
-        "excerpt":"IE pour developpement VR projet N-Hum-Inno.   ","categories": [],
-        "tags": ["Acceptability assessment","User eXperience","Living Lab"],
+        "excerpt":"Julien is an Development Engineer in particular he works on the development of the software solution for VR assessment in the project.   ","categories": [],
+        "tags": ["VR","Dev"],
         "url": "http://localhost:4000/people/julien-michaux/",
         "teaser":"http://localhost:4000/assets/images/people/JM.png"},{
         "title": "Laurent Dupont",
@@ -45,8 +45,8 @@ var store = [{
         "url": "http://localhost:4000/people/Mauricio-Camargo/",
         "teaser":"http://localhost:4000/assets/images/people/Mauricio-Camargo.jpg"},{
         "title": "Nicolas Ferveur",
-        "excerpt":"(Evaluation de l’acceptabilité, aide à la décision)   ","categories": [],
-        "tags": ["Acceptability assessment","User eXperience","Living Lab"],
+        "excerpt":"Nicolas is the R&amp;D manager of TEA. He is responsible for all the technological developments and the decision making about the technological solutions that goes in the software and hardware products of TEA.   ","categories": [],
+        "tags": ["User eXperience","Living Lab"],
         "url": "http://localhost:4000/people/nicolas-ferveur/",
         "teaser":"http://localhost:4000/assets/images/people/NF.png"},{
         "title": "Thibaut Muller",
@@ -128,4 +128,9 @@ var store = [{
         "excerpt":"During 23-24 September 2019 the N-Hum-Inno partners with the Univerisity of Lorraine will held a workshop with partners form the University of Tohoku (Japan) to share research results and strengthen collaboration links.   The program for this two day workshop is available here  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/posts/2019/Workshop-Tohoku/",
+        "teaser":null},{
+        "title": "N-hum-inno at Laval Virtual 2020",
+        "excerpt":"The Laval Virtual 2020 is being held virtually on Laval Virtual World this year during April 22-24 2020. N-Hum-Inno researchers Giovanny Arbelaez and Anaëlle Hily will present some works developed in the project: Anaëlle will present the paper untitled “Reliability of virtual reality for user experience in spatial cognition: an...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/posts/2020/laval-virtual-2020/",
         "teaser":null},]
